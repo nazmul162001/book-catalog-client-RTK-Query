21616036862c3db1df532441e1dd27b88bc299f6
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-17 22:52:56 +0600

    update live link

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000'
-// export const BASE_URL = 'https://book-catalog-backend-eight.vercel.app'
+// export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = 'https://book-catalog-backend-eight.vercel.app'
